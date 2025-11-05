22d738755bca05c2e4df0d415db9cdc03af098ae
mikky <mikky999@mail.ru>
2025-11-05 20:02:51 +0300

    done

--- src/main/java/ru/hogwarts/school_1/service/StudentService.java
+++ src/main/java/ru/hogwarts/school_1/service/StudentService.java
@@ -17,7 +17,7 @@ import java.util.OptionalDouble;
 public class StudentService {
     @Autowired
     private StudentRepository studentRepository;
-
+//добавлено сообщение
     public Optional<Faculty> getFacultyOfStudent(Long studentId) {
         log.debug("Invoked method 'getFacultyOfStudent' with parameter {}", studentId);
         Student student = studentRepository.findById(studentId).orElse(null);
@@ -28,51 +28,51 @@ public class StudentService {
             return Optional.empty();
         }
     }
-
+    //добавлено сообщение
     @Transactional
     public Student addStudent(Student student) {
         log.info("Invoked method 'addStudent'");
         return studentRepository.save(student);
     }
 
-
+    //добавлено сообщение
     public Optional<Student> findStudent(Long id) {
         log.debug("Invoked method 'findStudent' with parameter {}", id);
         return studentRepository.findById(id);
     }
-
+    //добавлено сообщение
     @Transactional
     public Student editStudent(Student updatedStudent) {
         log.info("Invoked method 'editStudent' с new data {}", updatedStudent);
         return studentRepository.save(updatedStudent);
     }
-
+    //добавлено сообщение
     @Transactional
     public void deleteStudent(Long id) {
         log.info("Invoked method 'deleteStudent' с id={}", id);
         studentRepository.deleteById(id);
     }
-
+    //добавлено сообщение
     public List<Student> findByAge(Integer age) {
         log.debug("Invoked method 'findByAge' с parameter {}", age);
         return studentRepository.findAllByAge(age);
     }
-
+    //добавлено сообщение
     public List<Student> findByAgeBetween(Integer minAge, Integer maxAge) {
         log.debug("Invoked method 'findByAgeBetween' с parameters {} to {}", minAge, maxAge);
         return studentRepository.findByAgeBetween(minAge, maxAge);
     }
-
+    //добавлено сообщение
     public List<AllStudent> countAllStudent() {
         log.info("Invoked method 'countAllStudent'");
         return studentRepository.countAllStudent();
     }
-
+    //добавлено сообщение
     public List<AllStudent> findAvarageAge() {
         log.info("Invoked method 'findAvarageAge'");
         return studentRepository.findAvarageAge();
     }
-
+    //добавлено сообщение
     public List<AllStudent> findFiveLastStudent() {
         log.info("Invoked method 'findFiveLastStudent'");
         return studentRepository.findFiveLastStudent();
